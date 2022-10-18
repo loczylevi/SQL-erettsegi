50a7e2b34eba7f13835043ef80fefdeba90c89cb
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2022-10-18 12:16:45 +0200

    Update vasut.sql

--- vasut.sql
+++ vasut.sql
@@ -5,3 +5,11 @@ Magyarországon kívül található állomások nevét és ország jelét, az á
 ábécérendben! (3kulfold)*/
 
 SELECT allomas.nev, allomas.orszag FROM allomas WHERE allomas.orszag <> "" ORDER BY allomas.nev;
+
+
+/*4. A 80-as vonal az egyik leghosszabb hazánkban. Készítsen lekérdezést, amely a kiinduló
+állomástól mért távolság sorrendjében megjeleníti a működő állomások nevét, típusát és az
+indulási állomástól mért távolságát! (480)*/
+
+SELECT allomas.nev, allomas.tipus, hely.tav FROM allomas,hely WHERE hely.vonalid = "80" AND allomas.mukodo <> '-1' AND allomas.id = hely.allomasid ORDER BY hely.tav;
+
